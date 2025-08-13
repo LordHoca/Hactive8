7ef08fd5660cc18c26a868f7236b0362c00934ca
LordHoca <lordhoca1@gmail.com>
2025-08-13 11:03:02 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+const body = document.body;
+let h1 = document.createElement('h1')
+h1.innerHTML = '<marquee>Welcome To Calculate Me!</marquee>' //Membuat tulisan 'Welcome To Calculate Me!' yang bergerak
+body.appendChild(h1)
+
+const tampilan = document.getElementById('tampilan'); // Mengambil elemen input dari html dengan id 'tampilan'
+
+function masukanKetampilan(input) {
+    tampilan.value += input; // fungsi untuk menambahkan input ke dalam tampilan kalkulator
+}
+
+function hapusAll() {
+    tampilan.value = ''; // fungsi untuk menghapus semua input pada tampilan kalkulator
+}
+
+function hitung() { // fungsi untuk menghitung hasil dari input yang ada pada tampilan kalkulator
+    try {
+        tampilan.value = eval(tampilan.value); // eval disini digunakan untuk mengoperasikan operasi matematika
+    }
+    catch (error) {
+        tampilan.value = 'Error'; // jika terjadi error pada operasi matematika, maka akan menampilkan 'Error' pada tampilan kalkulator
+    }
+}
+
+function hapus () {
+    tampilan.value = tampilan.value.slice(0, -1); // fungsi untuk menghapus karakter terakhir pada tampilan kalkulator
+}
